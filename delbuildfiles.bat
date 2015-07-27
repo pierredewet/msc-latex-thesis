5c02f6d227268afe28ebbb24270aa08fa933b9e6
Pierre de Wet <pdewet.bitc@gmail.com>
2015-07-27 12:45:37 +0100

    Updated style
    Updates style to more closely follow guidelines and included macros to
    automate document field population

--- delbuildfiles.bat
+++ delbuildfiles.bat
@@ -1,9 +1,5 @@
 @ECHO OFF
 del *.aux *.bbl *.log *.out *.gz *.toc *.blg *.(busy) *.gz
-move /Y EBSY705_cwk01_report.pdf ../final/
-COPY /Y *.tex ..\final\src\
-COPY /Y *.bib ..\final\src\
-COPY /Y *.png ..\final\src\
-COPY /Y *.pl ..\final\src\
+move /Y Thesis.pdf ../final/
 del *.pdf
 EXIT
